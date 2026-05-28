04124a1fa27771a27018f0e6358a60c6f2a883a0
2496736 <2496736@LTIN732874.cts.com>
2026-05-28 08:52:53 +0530

    correction in flightSErvice

--- travel360endpoints-3/src/main/java/com/cts/serviceimpl/FlightServiceImpl.java
+++ travel360endpoints-3/src/main/java/com/cts/serviceimpl/FlightServiceImpl.java
@@ -14,52 +14,42 @@ import java.util.List;
 @AllArgsConstructor
 public class FlightServiceImpl implements FlightService {
 
-    private final FlightRepository repo;
+	private final FlightRepository repo;
 
-    public Flight addFlight(FlightDTO dto) {
+	public Flight addFlight(FlightDTO dto) {
 
-        Flight flight = Flight.builder()
-                .flightNumber(dto.getFlightNumber())
-                .airlineName(dto.getAirlineName())
-                .source(dto.getSource())
-                .destination(dto.getDestination())
-                .arrivalTime(dto.getArrivalTime())
-                .flightDate(dto.getFlightDate())
-                .departureTime(dto.getDepartureTime())
-                .arrivalTime(dto.getArrivalTime())
-//                .availableSeats(dto.getTotalSeats())
-                .totalSeats(dto.getTotalSeats())
-                .price(dto.getPrice())
-                .status(dto.getStatus())
-                .build();
+		Flight flight = Flight.builder().flightNumber(dto.getFlightNumber()).airlineName(dto.getAirlineName())
+				.source(dto.getSource()).destination(dto.getDestination()).arrivalTime(dto.getArrivalTime())
+				.flightDate(dto.getFlightDate()).departureTime(dto.getDepartureTime()).arrivalTime(dto.getArrivalTime())
 
-        return repo.save(flight);
-    }
-@Override
-    public List<Flight> searchFlights(String source, String destination) {
-        return repo.findBySourceAndDestination(source, destination);
-    }
-@Override
-    public List<Flight> getAllFlights() {
-        return repo.findAll();
-    }
-@Override
-    public Flight getFlightById(Long id) {
-        return repo.findById(id).orElse(null);
-    }
-    
-    @Override
-    public List<Flight> filterFlights(String source, String destination, Double min, Double max) {
+				.totalSeats(dto.getTotalSeats()).price(dto.getPrice()).status(dto.getStatus()).build();
 
-        
-        if (min != null && max != null) {
-            return repo.findBySourceAndDestinationAndPriceBetween(source, destination, min, max);
-        }
+		return repo.save(flight);
+	}
 
-        
-        return repo.findBySourceAndDestination(source, destination);
-    }
-    
-    
+	@Override
+	public List<Flight> searchFlights(String source, String destination) {
+		return repo.findBySourceAndDestination(source, destination);
+	}
+
+	@Override
+	public List<Flight> getAllFlights() {
+		return repo.findAll();
+	}
+
+	@Override
+	public Flight getFlightById(Long id) {
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public List<Flight> filterFlights(String source, String destination, Double min, Double max) {
+
+		if (min != null && max != null) {
+			return repo.findBySourceAndDestinationAndPriceBetween(source, destination, min, max);
+		}
+
+		return repo.findBySourceAndDestination(source, destination);
+	}
 
 }
